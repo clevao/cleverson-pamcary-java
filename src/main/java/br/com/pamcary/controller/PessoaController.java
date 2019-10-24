d2dc9d5172c27daaab37d5996746cdd97191f0c0
Cleverson <cleverson.melo@outlook.com>
2019-10-24 01:15:51 -0300

    Habilitar o CROSS Origin

--- src/main/java/br/com/pamcary/controller/PessoaController.java
+++ src/main/java/br/com/pamcary/controller/PessoaController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.pamcary.entity.Pessoa;
 import br.com.pamcary.service.PessoaService;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class PessoaController {
     
